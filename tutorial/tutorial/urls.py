f417af66c075c5a251e71830f39d55f6916d6be3
Jason Mize <jasonemize@gmail.com>
2016-10-10 19:08:31 -0400

    part 4 finished

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -19,6 +19,10 @@ urlpatterns = [
     url(r'^', include('snippets.urls')),
 ]
 
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
 
 
 
